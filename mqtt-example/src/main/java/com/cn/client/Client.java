e59eefddbd7979f6b01c3abbce6565efe63e248b
xiaozhenguo <136325220@qq.com>
2020-02-21 19:34:01 +0800

    Revert 测试
    This reverts commit da5b3ce8a6e0dea17077f25d4a0e3bdacc4530c6.

--- mqtt-example/src/main/java/com/cn/client/Client.java
+++ mqtt-example/src/main/java/com/cn/client/Client.java
@@ -7,7 +7,6 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
 import java.util.UUID;
 
 /**
